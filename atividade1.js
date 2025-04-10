dc7439b4a41c0514766798448d2e50493aa70529
jujubfs <julia.bitencourt@estudante.ifro.edu.br>
2025-04-10 10:39:20 -0400

    Update de dados

---
+++ atividade1.js
@@ -0,0 +1,31 @@
+// Atividade 1
+
+let nome = "Julia Vitória";
+let idade = "15";
+
+console.log("Olá, meu nome é", nome ,"e eu tenho", idade ,"anos");
+
+// Atividade 2
+let string = "Julia Vitória";
+
+lower = string.toLowerCase();
+console.log(lower);
+
+upper = string.toUpperCase();
+console.log(upper);
+
+
+// Atividade 3
+let num = "10";
+let int = 10;
+
+let text = (typeof num);
+console.log(text);
+let numero = (typeof int);
+console.log(numero);
+
+// Atividade 4
+let peso = 44;
+let altura = 1.54;
+let imc = peso / (altura * altura);
+console.log(imc);
